ce5e689de74de617b7684c7db696f13c380e08eb
Rafael <rfalconi@vlacademy.com>
2022-09-13 21:55:51 -0500

    acer dinamico el producto

--- js/item.js
+++ js/item.js
@@ -1,2 +1,14 @@
-let item = JSON.parse(localStorage.getItem('item'));
-console.log(item);
+$(document).ready(function () {
+        let item = JSON.parse(localStorage.getItem('item'));
+        $("#img-principal").attr("src", item.img);
+        let html = '';
+        item.pictures.forEach((picture) => {
+            html += ' <div className="col-xl-2 col-md-4 col-sm-12">';
+            html += ' <img src="' + picture + '"   class="img-fluid">';
+            html += '</div>';
+        });
+        $('#pictures').append(html);
+        $('#name').append(item.name);
+
+    }
+)
